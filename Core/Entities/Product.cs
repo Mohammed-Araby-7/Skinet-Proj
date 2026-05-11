4fa54c3f27534e0b0a5dd0becb48633bfd65aba1
mohamedaraby <mohamed-araby-777@cloud.net>
2026-05-12 00:03:32 +0300

    feat: initialize skinet project with core, infrastructure, and api layers
    Implement a multi-layered .NET 9 architecture including:
    - Core layer: defines base entities and product domain models
    - Infrastructure layer: implements Entity Framework Core with SQL Server and product configurations
    - API layer: provides RESTful endpoints for product management using ASP.NET Core controllers and Swagger documentation

---
+++ Core/Entities/Product.cs
@@ -0,0 +1,12 @@
+﻿namespace Core.Entities;
+
+public class Product : BaseEntity
+{
+    public required string Name {get;set;}
+    public required string Description {get;set;}
+    public required decimal Price {get;set;}
+    public required string PicturalUrl {get;set;}
+    public required string Type {get;set;}
+    public required string Brand {get;set;}
+    public required string QuantityInStock {get;set;}
+}
