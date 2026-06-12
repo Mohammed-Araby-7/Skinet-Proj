f9440ef59605995b4ce09bff095414c9d3bfa902
mohamedaraby <mohamed-araby-777@cloud.net>
2026-06-12 23:18:20 +0300

    End_Of_section_8

--- client/src/app/app.ts
+++ client/src/app/app.ts
@@ -1,12 +1,36 @@
-import { Component, signal } from '@angular/core';
+import { Component, inject, OnInit, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from "./layout/header/header.component";
+import { HttpClient } from '@angular/common/http';
+import { Product } from './models/product';
+import { Pagination } from './models/pagination';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HeaderComponent],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
-export class App {
+export class App  implements OnInit{
+  baseUrl = 'https://localhost:5001/api/'
+
+
+  private http = inject(HttpClient);
+  products :Product[] = []
+  ngOnInit(): void {
+      this.http.get<Pagination<Product>>(this.baseUrl + 'products').subscribe({
+        next:(res)=>{
+          console.log(res);
+          this.products = res.data;
+        },
+        error:(err)=>{
+          console.log(err);
+        },
+        complete:()=>{
+          console.log('complete')
+        }
+      })
+  }
+
   protected title = 'Skient';
 }
